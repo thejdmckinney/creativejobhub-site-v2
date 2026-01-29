ecd251e075539354466dfd45f70c185eef6e8ec7
thejdmckinney <jeremy.mckinney@creativeconstructorsllc.com>
2026-01-28 18:12:18 -0600

    Add lists support to blog post schema
    - Added bullet and numbered list support to Sanity schema
    - Lists were showing in Sanity editor but not being saved/rendered
    - Added H4 heading style for more content hierarchy
    - Now bullet/numbered lists will properly save and display

--- sanity/schemas/blogPost.ts
+++ sanity/schemas/blogPost.ts
@@ -70,7 +70,12 @@ export const blogPost = defineType({
             { title: 'Normal', value: 'normal' },
             { title: 'H2', value: 'h2' },
             { title: 'H3', value: 'h3' },
+            { title: 'H4', value: 'h4' },
             { title: 'Quote', value: 'blockquote' },
+          ],
+          lists: [
+            { title: 'Bullet', value: 'bullet' },
+            { title: 'Numbered', value: 'number' },
           ],
           marks: {
             decorators: [
